61e735131ef7845a117ebfa1cfb59313d5ccdcbf
Paweł Krupa <paulfantom@gmail.com>
2017-11-30 23:40:35 +0100

    Update generatetag.sh

--- .travis/generatetag.sh
+++ .travis/generatetag.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Test if current commit is already tagged
+[[ $(git tag --points-at) ]] && exit 0
+
 git config --global user.email "paulfantom@gmail.com"
 git config --global user.name "paulfantom"
 GIT_TAG=$([[ "$TRAVIS_COMMIT_MESSAGE" =~ ("Merge pull request".*/feature.*) ]] && git semver --next-minor || git semver --next-patch )
@@ -8,4 +11,4 @@ git tag $GIT_TAG -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMB
 GIT_URL=$(git config --get remote.origin.url)
 GIT_URL=${GIT_URL#*//}
 
-git push https://${GH_TOKEN}:@${GIT_URL} --tags || exit 0
+git push https://${GH_TOKEN}:@${GIT_URL} --tags
