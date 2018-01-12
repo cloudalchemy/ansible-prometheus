bb752fc72c3b21dc4201f0ebc9e0c461844b3257
paulfantom <pawel@krupa.net.pl>
2018-01-12 09:27:14 +0100

    use tagged images

--- images.sh
+++ images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in ubuntu-molecule:16.04 debian-molecule:9 debian-molecule:8 centos-molecule fedora-molecule
+for i in ubuntu-molecule:16.04 debian-molecule:9 debian-molecule:8 centos-molecule:7 fedora-molecule:27
 do 
     docker pull paulfantom/$i &
 done
