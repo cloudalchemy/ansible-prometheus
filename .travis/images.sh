38b2d0b6e154c924b852833d1fa781cde535412a
paulfantom <pawel@krupa.net.pl>
2018-03-21 23:53:07 +0100

    predownload bionic image

--- .travis/images.sh
+++ .travis/images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in ubuntu-molecule:16.04 debian-molecule:9 debian-molecule:8 centos-molecule:7 fedora-molecule:27
+for i in ubuntu-molecule:18.04 ubuntu-molecule:16.04 debian-molecule:9 debian-molecule:8 centos-molecule:7 fedora-molecule:27
 do 
     docker pull paulfantom/$i &
 done
