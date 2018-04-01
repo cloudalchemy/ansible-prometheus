ea5cc6e8a9897429b5355fd5a5f2a1f8bc228f0a
paulfantom <pawel@krupa.net.pl>
2018-04-01 23:27:00 +0200

    fix indentation

--- tests/test_default.py
+++ tests/test_default.py
@@ -46,8 +46,8 @@ def test_socket(host):
 def test_version(host):
     v = os.getenv('PROMETHEUS', DEFAULT_VERSION)
     if int(v[0]) < 2:
-      out = host.run("/usr/local/bin/prometheus -version").stderr
+        out = host.run("/usr/local/bin/prometheus -version").stderr
     else:
-      out = host.run("/usr/local/bin/prometheus --version").stderr
+        out = host.run("/usr/local/bin/prometheus --version").stderr
     version = "prometheus, version " + v
     assert version in out
