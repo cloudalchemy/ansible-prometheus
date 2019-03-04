e428fd93cc69bae42545a4f34249ee3686ad0ca3
paulfantom <pawel@krupa.net.pl>
2019-03-04 18:30:46 +0100

    [minor] cleanup after test

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-	tox -- molecule test --all --destroy never
+	tox -- molecule test --all
 else
 	tox -- molecule test -s default --destroy always
 	tox -- molecule test -s alternative --destroy never
