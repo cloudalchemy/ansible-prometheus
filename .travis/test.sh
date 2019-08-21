2df701a5cf2d8f50e15414199999b46b451d7749
paulfantom <pawel@krupa.net.pl>
2019-08-21 23:00:09 +0200

    simplify running tests scenario on master branch

--- .travis/test.sh
+++ .travis/test.sh
@@ -8,7 +8,7 @@ if [ ! -d "./molecule/latest" ]; then
 fi
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-	tox -- molecule test --all --destroy never
+	tox -- molecule test --all
 else
 	tox -- molecule test -s default --destroy always
 	if [ -d "./molecule/alternative" ]; then
