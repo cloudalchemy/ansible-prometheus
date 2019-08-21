d88e8911ac3925aea47a4b9a337046dd30d14e7f
Paweł Krupa <pawel@krupa.net.pl>
2019-08-21 18:05:51 +0200

    add RHEL8 and debian buster support; remove testing on debian je… (#226)

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+set -euo pipefail
+
+if [ ! -d "./molecule/latest" ]; then
+	tox
+	exit 0
+fi
+
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-	tox -- molecule test --all
+	tox -- molecule test --all --destroy never
 else
 	tox -- molecule test -s default --destroy always
-	tox -- molecule test -s alternative --destroy never
+	if [ -d "./molecule/alternative" ]; then
+		tox -- molecule test -s alternative --destroy never
+	fi
 fi
